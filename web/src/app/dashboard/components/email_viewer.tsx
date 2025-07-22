97db3663e04551211415e46b00da48918c3d50b8
gagewillette <79120652+gagewillette@users.noreply.github.com>
2025-07-22 12:31:11 -0400

    fix

--- web/src/app/dashboard/components/email_viewer.tsx
+++ web/src/app/dashboard/components/email_viewer.tsx
@@ -91,7 +91,7 @@ function SubHeader({ data }: { data: EmailContextType }) {
 
         <button
           type="button"
-          onClick={refreshHandler}
+          onClick={pollHandler}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900"
         >
           Poll New Emails
